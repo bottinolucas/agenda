75d202d2653fb9b8925e9eaddb4941c2cb51944b
bottinolucas <lucasgabottino@gmail.com>
2025-07-10 11:40:47 -0300

    feat/contract-category: add relationship with category

--- contact/admin.py
+++ contact/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact.models import Contact, Category
 
 # Register your models here.
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display= 'id', 'first_name', 'last_name', 'email'
+    list_display= 'id', 'first_name', 'last_name', 'email', 'category'
     ordering='id',
     search_fields= 'id', 'first_name', 'last_name'
     list_per_page=10
     list_max_show_all=200
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display= 'name',
