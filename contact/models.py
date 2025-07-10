78f5f6abc54d4f6d9f8f26e884a6a644bcb3ee54
bottinolucas <lucasgabottino@gmail.com>
2025-07-10 11:27:56 -0300

    feat/contact: adding image field and setting media_url and media_root

--- contact/models.py
+++ contact/models.py
@@ -9,7 +9,9 @@ class Contact(models.Model):
     phone = models.CharField(max_length=11, blank=True)
     email = models.EmailField(max_length=254)
     created_date = models.DateTimeField(default=timezone.now)
-    description = models.TextField()
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
